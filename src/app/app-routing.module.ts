1d70f044935cb05e3dfba770d55eb5961c1d10cf
sekcess <itsalifanugrah@gmail.com>
2021-11-17 01:48:34 +0700

    update hasil uts semester 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TournamentComponent } from './tournament/tournament.component';
 
+
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  {path:'leaderboard', component: LeaderboardComponent},
-  {path:'profile', component: ProfileComponent},
-  {path:'tournament', component: TournamentComponent},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'tournament', component: TournamentComponent},
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'home', component: HomeComponent }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
